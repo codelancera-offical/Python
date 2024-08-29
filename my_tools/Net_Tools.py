e5958f76f33e90c7cf32808ae2c1a1a1c85e1700
CodelancerA <lancera.thn@gmail.com>
2024-08-29 12:10:48 -0700

    Complete get_ip_on_linux and ssh connect scripts in python

---
+++ my_tools/Net_Tools.py
@@ -0,0 +1,55 @@
+import subprocess
+import re
+
+def get_ip_on_linux():
+    result = subprocess.run(['ifconfig'], capture_output=True, text=True)
+    output = result.stdout
+
+    match = re.search(r'wlan0:.*?inet (\d+\.\d+\.\d+\.\d+)', output, re.DOTALL)
+
+    if match:
+        return match.group(1)
+    else:
+        return "No IP address found for wlan0"
+    
+def connect_to_termux():
+    private_key_path = r"C:\Users\30752\.ssh\id_rsa"
+    username = "u0_a53"
+    host = input("please enter termux's ip: ")
+    port = 8022
+
+    # build ssh connect command
+    command = [
+        "ssh",
+        "-i", private_key_path,
+        f"{username}@{host}",
+        "-p", str(port)
+    ]
+
+    try:
+        subprocess.run(command, check=True)
+    except subprocess.CalledProcessError as e:
+        print(f"SSH connect failed: {e}")
+    
+def main():
+    functions = {
+        "0": get_ip_on_linux,
+        "1": connect_to_termux,
+    }
+
+    print("All net operations:")
+    for key, value in functions.items():
+        print(f"\t{key}: {value.__name__}") # print name of function
+
+    choice = input("Choose a net operation: ")
+
+    if choice in functions:
+        result = functions[choice]()
+        if result is not None:
+            print(result)
+        else:
+            print("Invalid choice. Please select a valid option.")
+
+
+if __name__ == "__main__":
+    main()
