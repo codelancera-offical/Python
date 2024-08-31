f2b9a72c45df03b36cf604fc7eaa5f0c17958de5
CodelancerA <lancera.thn@gmail.com>
2024-08-30 22:33:46 -0700

    Add AI_Tools

--- my_tools/AI_Tools.py
+++ my_tools/AI_Tools.py
@@ -1,4 +1,6 @@
 from openai import OpenAI
+from colorama import Fore
+import webbrowser
 
 
 def generate_text(choose_model="gpt-4o-mini", user_input="Randomly tell me something, news, jokes, movie recommendations, or some terminal or computer tricks, just like the start screen of windows."):
@@ -14,15 +16,40 @@ def generate_text(choose_model="gpt-4o-mini", user_input="Randomly tell me somet
             {
                 "role": "user",
                 "content": user_input
-
             }
         ]
     )
+    """ content example
+        {
+    "choices": [
+        {
+        "finish_reason": "stop",
+        "index": 0,
+        "message": {
+            "content": "The 2020 World Series was played in Texas at Globe Life Field in Arlington.",
+            "role": "assistant"
+        },
+        "logprobs": null
+        }
+    ],
+    "created": 1677664795,
+    "id": "chatcmpl-7QyqpwdfhqwajicIEznoc6Q47XAyW",
+    "model": "gpt-4o-mini",
+    "object": "chat.completion",
+    "usage": {
+        "completion_tokens": 17,
+        "prompt_tokens": 57,
+        "total_tokens": 74
+    }
+    }
+    """
 
-    print(completion.choices[0].message)
+    print(Fore.GREEN + "----------GPT Response-----------")
+    print(Fore.GREEN + completion.choices[0].message.content)
+    print(Fore.GREEN + "---------------------------------")
 
 
-def generate_image(user_prompt="A peaceful sea."):
+def generate_image(user_prompt="A peaceful sea.", display=False):
     client = OpenAI()
 
     response = client.images.generate(
@@ -31,4 +58,9 @@ def generate_image(user_prompt="A peaceful sea."):
         size="1024x1024"
     )
 
-    print(response.data[0].url)
+    if display == False:
+        print(response.data[0].url)
+    else:
+        webbrowser.open(response.data[0].url)
+        
+
---
+++ my_tools/IO_Tools.py
@@ -0,0 +1,62 @@
+import time
+import sys
+from colorama import Fore, Style, init
+
+
+def print_slowly(text=None, color="white", delay=0.05):
+    init(autoreset=False)
+
+
+    # Define a dictionary to map simple color names to colorama codes
+    color_map = {
+        "black": Fore.BLACK,
+        "red": Fore.RED,
+        "green": Fore.GREEN,
+        "yellow": Fore.YELLOW,
+        "blue": Fore.BLUE,
+        "magenta": Fore.MAGENTA,
+        "cyan": Fore.CYAN,
+        "white": Fore.WHITE,
+        "reset": Style.RESET_ALL
+    }
+
+    color_code = color_map.get(color.lower(), Fore.White)   # default is white
+    sys.stdout.write(color_code)
+    sys.stdout.flush()
+
+    if text is None:
+        text = input("Please input some text: ")
+
+    for char in text:
+        sys.stdout.write(char)
+        sys.stdout.flush()
+        time.sleep(delay)
+
+    sys.stdout.write(Style.RESET_ALL)
+    sys.stdout.flush()
+
+
+    sys.stdout.write("\n")
+    return True
+
+def main():
+    functions = {
+        "0": print_slowly
+    }
+
+    print("All IO tools example: ")
+    for key, value in functions.items():
+        print(f"\t{key}: {value.__name__}") # print name of function
+
+    choice = input("Choose a net operation: ")
+
+    if choice in functions:
+        result = functions[choice]()
+        if result is not None:
+            print(result)
+        else:
+            print("Invalid choice. Please select a valid option.")
+
+
+if __name__ == "__main__":
+    main()
