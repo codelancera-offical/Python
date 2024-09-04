af8d2c0fcce3ea77aa6394786611fe6e2a1511b1
codelancera-phone <lancera.thn@gmail.com>
2024-09-03 21:26:11 -0700

    scan tools

--- my_tools/Hack_Tools.py
+++ my_tools/Hack_Tools.py
@@ -22,4 +22,15 @@ def get_ip_range(interface='wlan0'):
     except Exception as e:
         print(f"can not get IP range: {e}")
         return None
-    
+
+
+def scan_network(ip_range):
+
+    nm = nmap.PortScanner()
+    try:
+        print(f"Scanning IP range: {ip_range}")
+        nm.scan(hosts=ip_range, arguments='-sn')  # 使用 -sn 执行主机发现（ping 扫描）
+        for host in nm.all_hosts():
+            print(f"host: {host}, status: {nm[host].state()}")
+    except Exception as e:
+        print(f"error in scanning: {e}")
